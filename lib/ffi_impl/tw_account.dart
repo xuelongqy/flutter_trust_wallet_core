130bc88752f4d0da440940bed751d87c0be58d87
memtopia <memtopia@gmail.com>
2021-07-21 14:41:17 +0800

    impl and update .gitignore

--- lib/ffi_impl/tw_account.dart
+++ lib/ffi_impl/tw_account.dart
@@ -2,14 +2,7 @@ part of trust_wallet_core;
 
 /// trust wallet core
 class TWAccount extends Opaque {
-  /// Account for a particular coin within a wallet.
-  late final Pointer<Int32> _TW_EXPORT_CLASS = lookup<Int32>('TW_EXPORT_CLASS');
-
-  int get TW_EXPORT_CLASS => _TW_EXPORT_CLASS.value;
-
-  set TW_EXPORT_CLASS(int value) => _TW_EXPORT_CLASS.value = value;
-
-  Pointer<Int32> TWAccountCreate(
+  static Pointer<Int32> TWAccountCreate(
     Pointer<Int32> address,
     int coin,
     Pointer<Int32> derivationPath,
@@ -23,11 +16,11 @@ class TWAccount extends Opaque {
     );
   }
 
-  late final _TWAccountCreate_ptr = lookup<NativeFunction<_c_TWAccountCreate>>('TWAccountCreate');
-  late final _dart_TWAccountCreate _TWAccountCreate = _TWAccountCreate_ptr.asFunction<_dart_TWAccountCreate>();
+  static late final _TWAccountCreate_ptr = lookup<NativeFunction<_c_TWAccountCreate>>('TWAccountCreate');
+  static late final _dart_TWAccountCreate _TWAccountCreate = _TWAccountCreate_ptr.asFunction<_dart_TWAccountCreate>();
 
-  int TWAccountDelete(
-    Pointer<TWAccount> account,
+  void TWAccountDelete(
+    Pointer<Void> account,
   ) {
     return _TWAccountDelete(
       account,
@@ -37,14 +30,41 @@ class TWAccount extends Opaque {
   late final _TWAccountDelete_ptr = lookup<NativeFunction<_c_TWAccountDelete>>('TWAccountDelete');
   late final _dart_TWAccountDelete _TWAccountDelete = _TWAccountDelete_ptr.asFunction<_dart_TWAccountDelete>();
 
-  late final Pointer<Int32> _TWString = lookup<Int32>('TWString');
+  Pointer<Utf8> TWAccountAddress(
+    Pointer<Void> account,
+  ) {
+    return _TWAccountAddress(
+      account,
+    );
+  }
 
-  int get TWString => _TWString.value;
+  late final _TWAccountAddress_ptr = lookup<NativeFunction<_c_TWAccountAddress>>('TWAccountAddress');
+  late final _dart_TWAccountAddress _TWAccountAddress = _TWAccountAddress_ptr.asFunction<_dart_TWAccountAddress>();
 
-  set TWString(int value) => _TWString.value = value;
+  Pointer<Utf8> TWAccountDerivationPath(
+    Pointer<Void> account,
+  ) {
+    return _TWAccountDerivationPath(
+      account,
+    );
+  }
+
+  late final _TWAccountDerivationPath_ptr = lookup<NativeFunction<_c_TWAccountDerivationPath>>('TWAccountDerivationPath');
+  late final _dart_TWAccountDerivationPath _TWAccountDerivationPath = _TWAccountDerivationPath_ptr.asFunction<_dart_TWAccountDerivationPath>();
+
+  Pointer<Utf8> TWAccountExtendedPublicKey(
+    Pointer<Void> account,
+  ) {
+    return _TWAccountExtendedPublicKey(
+      account,
+    );
+  }
+
+  late final _TWAccountExtendedPublicKey_ptr = lookup<NativeFunction<_c_TWAccountExtendedPublicKey>>('TWAccountExtendedPublicKey');
+  late final _dart_TWAccountExtendedPublicKey _TWAccountExtendedPublicKey = _TWAccountExtendedPublicKey_ptr.asFunction<_dart_TWAccountExtendedPublicKey>();
 
   int TWAccountCoin(
-    Pointer<TWAccount> account,
+    Pointer<Void> account,
   ) {
     return _TWAccountCoin(
       account,
@@ -69,18 +89,42 @@ typedef _dart_TWAccountCreate = Pointer<Int32> Function(
   Pointer<Int32> extendedPublicKey,
 );
 
-typedef _c_TWAccountDelete = Int32 Function(
-  Pointer<TWAccount> account,
+typedef _c_TWAccountDelete = Void Function(
+  Pointer<Void> account,
 );
 
-typedef _dart_TWAccountDelete = int Function(
-  Pointer<TWAccount> account,
+typedef _dart_TWAccountDelete = void Function(
+  Pointer<Void> account,
+);
+
+typedef _c_TWAccountAddress = Pointer<Utf8> Function(
+  Pointer<Void> account,
+);
+
+typedef _dart_TWAccountAddress = Pointer<Utf8> Function(
+  Pointer<Void> account,
+);
+
+typedef _c_TWAccountDerivationPath = Pointer<Utf8> Function(
+  Pointer<Void> account,
+);
+
+typedef _dart_TWAccountDerivationPath = Pointer<Utf8> Function(
+  Pointer<Void> account,
+);
+
+typedef _c_TWAccountExtendedPublicKey = Pointer<Utf8> Function(
+  Pointer<Void> account,
+);
+
+typedef _dart_TWAccountExtendedPublicKey = Pointer<Utf8> Function(
+  Pointer<Void> account,
 );
 
 typedef _c_TWAccountCoin = Int32 Function(
-  Pointer<TWAccount> account,
+  Pointer<Void> account,
 );
 
 typedef _dart_TWAccountCoin = int Function(
-  Pointer<TWAccount> account,
+  Pointer<Void> account,
 );
