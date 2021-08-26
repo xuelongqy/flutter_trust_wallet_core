61ad886a5fcaaa30f09a7f7359940f37e4cde2c9
xuelongqy <xuelongqy@qq.com>
2021-08-26 17:00:45 +0800

    feat: 区分示例

---
+++ example/lib/bitcoin_address_example.dart
@@ -0,0 +1,39 @@
+import 'dart:typed_data';
+
+import 'package:convert/convert.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_trust_wallet_core/flutter_trust_wallet_core.dart';
+import 'package:flutter_trust_wallet_core_example/base_example.dart';
+
+class BitcoinAddressExample extends BaseExample {
+  final HDWallet wallet;
+
+  const BitcoinAddressExample(this.wallet, {Key? key}) : super('Ethereum', key: key);
+
+  @override
+  _BitcoinAddressExampleState createState() => _BitcoinAddressExampleState();
+}
+
+class _BitcoinAddressExampleState extends BaseExampleState<BitcoinAddressExample> {
+  @override
+  void initState() {
+    super.initState();
+    int coin = CoinType.BitcoinTestnet;
+    final privakye2 = widget.wallet.getKey(coin, "m/44'/0'/0'/0/0");
+    logger.d(hex.encode(privakye2.data()));
+    final publicKey2 = privakye2.getPublicKeySecp256k1(true);
+    final bitcoinAddress = BitcoinAddress.createWithPublicKey(publicKey2, coin);
+    logger.d(bitcoinAddress.description());
+    final segwitAddress = SegwitAddress.createWithPublicKey(1, publicKey2);
+    logger.d(segwitAddress.description());
+    final address2 = AnyAddress.createWithPublicKey(publicKey2, 0);
+    logger.d("keystore a = ${address2.description()}");
+
+    final pubKeyHash = Hash.hashSHA256RIPEMD(publicKey2.data());
+    final bitcoinScript = BitcoinScript.buildPayToWitnessPubkeyHash(pubKeyHash);
+    final scriptHash = Hash.hashSHA256RIPEMD(bitcoinScript.data());
+    final data = Uint8List.fromList([5]..addAll(scriptHash.toList()));
+    final bitcoinAddress2 = BitcoinAddress.createWithData(data);
+    logger.d(bitcoinAddress2.description());
+  }
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,6 +4,9 @@ import 'package:convert/convert.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_trust_wallet_core/flutter_trust_wallet_core.dart';
 import 'package:flutter_trust_wallet_core/trust_wallet_core_ffi.dart';
+import 'package:flutter_trust_wallet_core_example/bitcoin_address_example.dart';
+import 'package:flutter_trust_wallet_core_example/bitcoin_transaction_example.dart';
+import 'package:flutter_trust_wallet_core_example/ethereum_example.dart';
 import 'package:logger/logger.dart';
 import 'package:flutter_trust_wallet_core/protobuf/bitcoin.pb.dart' as Bitcoin;
 import 'package:fixnum/fixnum.dart' as $fixnum;
@@ -14,171 +17,88 @@ void main() {
   runApp(MyApp());
 }
 
-class MyConsoleOutput extends LogOutput {
-  final Function(String) myPrint;
-
-  MyConsoleOutput(this.myPrint);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  void output(OutputEvent event) {
-    event.lines.forEach(myPrint);
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Example(),
+    );
   }
 }
 
-class MylogFilter extends LogFilter {
+class Example extends StatefulWidget {
   @override
-  bool shouldLog(LogEvent event) {
-    return true;
-  }
+  _ExampleState createState() => _ExampleState();
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-void showLog(String log) {
-  logs.add(log);
-}
-
-class _MyAppState extends State<MyApp> {
-  var logger = Logger(
-    output: MyConsoleOutput(showLog),
-    filter: MylogFilter(),
-    printer: PrettyPrinter(
-      methodCount: 0,
-      errorMethodCount: 0,
-      lineLength: 30,
-      printEmojis: true,
-      printTime: false,
-    ),
-  );
+class _ExampleState extends State<Example> {
+  late HDWallet wallet;
 
   @override
   void initState() {
     FlutterTrustWalletCore.init();
     super.initState();
+    wallet = HDWallet();
+  }
+
+  Widget _exampleItem({
+    required String name,
+    required WidgetBuilder builder,
+  }) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: builder));
+      },
+      child: Text(name),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('wallet core example app'),
-        ),
-        body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                    itemBuilder: (context, index) {
-                      return Text(
-                        logs[index],
-                        style: TextStyle(fontSize: 12),
-                      );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('wallet core example app'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text(wallet.mnemonic()),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  _exampleItem(
+                    name: 'Ethereum',
+                    builder: (_) {
+                      return EthereumExample(wallet);
                     },
-                    itemCount: logs.length),
+                  ),
+                  _exampleItem(
+                    name: 'Bitcoin Address',
+                    builder: (_) {
+                      return BitcoinAddressExample(wallet);
+                    },
+                  ),
+                  _exampleItem(
+                    name: 'Bitcoin Transaction',
+                    builder: (_) {
+                      return BitcoinTransactionExample(wallet);
+                    },
+                  ),
+                ],
               ),
-              ElevatedButton(
-                  onPressed: () async {
-                    logs.clear();
-                    TWStellarMemoType.TWStellarMemoTypeHash;
-                    HDWallet wallet = HDWallet();
-                    logger.d("address ${wallet.getAddressForCoin(60)}");
-                    logger.d("mnemonic = ${wallet.mnemonic()}");
-                    print(wallet.mnemonic());
-                    String privateKeyhex = hex.encode(wallet.getKeyForCoin(60).data());
-                    // String privateKey0 = hex.encode(wallet.getDerivedKey(60,0,0,0).data());
-                    // String privateKey1 = hex.encode(wallet.getDerivedKey(60,0,0,1).data());
-                    logger.d("privateKeyhex = $privateKeyhex");
-                    // logger.d("privateKeyhex0 = $privateKey0");
-                    // logger.d("privateKeyhex1 = $privateKey1");
-                    logger.d("seed = ${hex.encode(wallet.seed())}");
-                    final a = StoredKey.importPrivateKey(wallet.getKeyForCoin(60).data(), "", Uint8List.fromList("123aaa".codeUnits), 60);
-                    logger.d("keystore a = ${a.exportJson()}");
-
-                    final publicKey = wallet.getKeyForCoin(60).getPublicKeySecp256k1(false);
-                    final anyAddress = AnyAddress.createWithPublicKey(publicKey, 60);
-
-                    // logger.d("1 = ${AnyAddress.isValid("0xfaC5482fffe86d33c3b8ADB24F839F5e60aF99d4", DartTWCoinType.TWCoinTypeEthereum)}");
-                    // logger.d("2 = ${AnyAddress.isValid("0xfaC5482fffe86d33c3b8ADB24F839F5e60af99d4", DartTWCoinType.TWCoinTypeEthereum)}");
-                    // logger.d("3 = ${AnyAddress.isValid("faC5482fffe86d33c3b8ADB24F839F5e60af99d4", DartTWCoinType.TWCoinTypeEthereum)}");
-                    final privakye = wallet.getKey(60, "m/49'/60'/0'/0/0");
-                    final publicKey1 = privakye.getPublicKeySecp256k1(true);
-                    final address = AnyAddress.createWithPublicKey(publicKey1, 0);
-
-                    logger.d("keystore a = ${address.description()}");
-
-                    // btc testnet
-                    final privakye2 = wallet.getKey(0, "m/44'/1'/0'/0/0");
-                    logger.d(hex.encode(privakye2.data()));
-                    final publicKey2 = privakye2.getPublicKeySecp256k1(true);
-                    final bitcoinAddress = BitcoinAddress.createWithPublicKey(publicKey2, 111);
-                    logger.d(bitcoinAddress.description());
-                    final segwitAddress = SegwitAddress.createWithPublicKey(2, publicKey2);
-                    logger.d(segwitAddress.description());
-                    final address2 = AnyAddress.createWithPublicKey(publicKey2, 1);
-                    logger.d("keystore a = ${address2.description()}");
-
-                    final pubKeyHash = Hash.hashSHA256RIPEMD(publicKey2.data());
-                    final bitcoinScript = BitcoinScript.buildPayToWitnessPubkeyHash(pubKeyHash);
-                    final scriptHash = Hash.hashSHA256RIPEMD(bitcoinScript.data());
-                    final data = Uint8List.fromList([196]..addAll(scriptHash.toList()));
-                    final bitcoinAddress2 = BitcoinAddress.createWithData(data);
-                    logger.d(bitcoinAddress2.description());
-
-                    // Transaction
-                    int coin = CoinType.BitcoinTestnet;
-                    final addressBtc = wallet.getAddressForCoin(coin);
-                    final toAddress = "tb1qks8l9haxjszn9r6yf2dm65ed3w6wmz85r379ms";
-                    final changeAddress = "mvcJcHN3ZhELVVhncfjARUMtfRELwhGXgL";
-                    final secretPrivateKeyBtc = wallet.getKeyForCoin(coin);
-                    final signingInput = Bitcoin.SigningInput(
-                      amount: $fixnum.Int64.parseInt('37000'),
-                      hashType: BitcoinScript.hashTypeForCoin(coin),
-                      toAddress: toAddress,
-                      changeAddress: changeAddress,
-                      byteFee: $fixnum.Int64.parseInt('10'),
-                      coinType: coin,
-                      utxo: [
-                        Bitcoin.UnspentTransaction(
-                          amount: $fixnum.Int64.parseInt('20000'),
-                          outPoint: Bitcoin.OutPoint(
-                            hash: hex.decode('1b23757cdc023b3ac9f033522abb9f845815b65cce1e25411e8ad950899c0e71').reversed.toList(),
-                            index: 0,
-                            sequence: 4294967295,
-                          ),
-                          script: BitcoinScript.lockScriptForAddress(addressBtc, coin).data().toList(),
-                        ),
-                        Bitcoin.UnspentTransaction(
-                          amount: $fixnum.Int64.parseInt('20000'),
-                          outPoint: Bitcoin.OutPoint(
-                            hash: hex.decode('7611002ff116fad20ef12ad30010a07d5b25edf37209504dd42a6a4c5c27aa75').reversed.toList(),
-                            index: 0,
-                            sequence: 4294967295,
-                          ),
-                          script: BitcoinScript.lockScriptForAddress(addressBtc, coin).data().toList(),
-                        ),
-                      ],
-                      privateKey: [
-                        secretPrivateKeyBtc.data().toList(),
-                      ],
-                    );
-                    final transactionPlan = Bitcoin.TransactionPlan.fromBuffer(AnySigner.signerPlan(signingInput.writeToBuffer(), coin).toList());
-                    print('availableAmount: ${transactionPlan.availableAmount} amount: ${transactionPlan.amount} fee: ${transactionPlan.fee} change: ${transactionPlan.change}');
-                    signingInput.plan = transactionPlan;
-                    signingInput.amount = transactionPlan.amount;
-                    final sign = AnySigner.sign(signingInput.writeToBuffer(), coin);
-                    final signingOutput = Bitcoin.SigningOutput.fromBuffer(sign);
-                    print(hex.encode(signingOutput.encoded));
-                    logger.d(hex.encode(signingOutput.encoded));
-
-                    setState(() {});
-                  },
-                  child: Text("gen")),
-            ],
-          ),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                wallet.delete();
+                wallet = HDWallet();
+                setState(() {});
+              },
+              child: Text("gen"),
+            ),
+          ],
         ),
       ),
     );
